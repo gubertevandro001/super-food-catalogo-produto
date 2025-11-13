fb3794a915ddd0c725eb7ccc3f3a569a3541023f
Evandro Gubert <gubertevandro33@gmail.com>
2025-11-12 21:14:14 -0300

    chore: subindo o projeto e configurando o banco de dados

---
+++ src/main/java/com/superfood/catalogo_produto/SuperFoodCatalogoProdutoApplication.java
@@ -0,0 +1,12 @@
+package com.superfood.catalogo_produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperFoodCatalogoProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperFoodCatalogoProdutoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/superfood/catalogo_produto/SuperFoodCatalogoProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superfood.catalogo_produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperFoodCatalogoProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
