97abe12304d661e3ec2f200509a45545804c895a
Evandro Gubert <gubertevandro33@gmail.com>
2025-12-01 21:59:44 -0300

    feature: adicionando o swagger na aplicacao

---
+++ src/main/java/com/superfood/catalogo_produto/config/SpringDocConfig.java
@@ -0,0 +1,18 @@
+package com.superfood.catalogo_produto.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("super-food-catalogo-produto-api")
+                        .description("API para gerenciar o catálogo de produtos do ERP Super Food")
+                        .version("1.0.0"));
+    }
+}
